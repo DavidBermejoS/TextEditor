859d8d11beb5adbe68479e977b6cf4b2997a7535
David Bermejo <dbermejosimon@gmail.com>
2019-02-10 14:12:04 +0100

    Finalizada implementacion de botones de barra de herramientas (Abrir fichero, guardar, guardar como)

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # TextEditor
 Editor de Texto sencillo Realizado para el módulo de Desarrollo de Interfaces en el ciclo de Desarrollo para Aplicaciones Multiplataforma
 
-####Actual Version = v0.2.0
+#### Actual Version = v0.2.0
 
-##ChangeLog
-- v0.1.0:
-1. Añadidas clases principales: Main, Window y File Manager.
-2. Añadidas fucionalidades Cargar archivo, guardar archivo y guardar como.
-3. Añadidos recursos pra implementación de barra de herramientas y diccionario.
+## ChangeLog
+1. __v0.1.0__:
+- Añadidas clases principales: Main, Window y File Manager.
+- Añadidas fucionalidades Cargar archivo, guardar archivo y guardar como.
+- Añadidos recursos pra implementación de barra de herramientas y diccionario.
 
-- v0.2.0:
-1. Comienzo de implementacion de barra de herramientas y añadidos botones de 
+2. __v0.2.0__:
+- Comienzo de implementacion de barra de herramientas y añadidos botones de 
 guardar y abrir fichero
+- Finalizada implementacion de botones de barra de herramientas (Abrir fichero, guardar, guardar como)
+
+
 
 
 
--- src/view/Window.java
+++ src/view/Window.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -31,6 +32,7 @@ public class Window {
 
     JButton buttonSave;
     JButton buttonLoad;
+    JButton buttonSaveAs;
 
     JTextArea textArea;
     FileManager fm;
@@ -96,19 +98,43 @@ public class Window {
         toolBar.setFloatable(false);
 
         //agregado de botones en el toolbar
-        Icon loadIcon = new ImageIcon("resources/imagenes/fileopen.png");
-        buttonLoad = new JButton(loadIcon);
-        buttonLoad.setSize(50,50);
+        buttonLoad = new JButton();
+        buttonLoad.setIcon(getIconImage("resources/imagenes/fileopen.png"));
         toolBar.add(buttonLoad);
 
-        Icon saveIcon = new ImageIcon("resources/imagenes/filesave.png");
-        buttonSave = new JButton(saveIcon);
+        buttonSave = new JButton();
+        buttonSave.setIcon(getIconImage("resources/imagenes/filesave.png"));
         toolBar.add(buttonSave);
+
+        buttonSaveAs = new JButton();
+        buttonSaveAs.setIcon(getIconImage("resources/imagenes/SaveAs.png"));
+        toolBar.add(buttonSaveAs);
 
 
         frame.add(toolBar,BorderLayout.NORTH);
     }
 
+    /**
+     * Metodo encargado de obtener la imagen de la carpeta de recursos y devolver un icono para los
+     * botones de la barra de herramientas
+     * @param s : String con la ruta a la imagen
+     * @return Icon icon : icono para el boton
+     */
+    private Icon getIconImage(String s) {
+        ImageIcon imageIcon = new ImageIcon(getClass().getClassLoader().getResource(s));
+        Image img = imageIcon.getImage();
+        BufferedImage bufferedImage = new BufferedImage(40,40,BufferedImage.TYPE_INT_ARGB);
+        Graphics g = bufferedImage.createGraphics();
+        g.drawImage(img,0,0,40,40,null);
+        imageIcon = new ImageIcon(bufferedImage);
+        return imageIcon;
+    }
+
+
+    /**
+     * Metodo encargado de gestionar los listener de los botones de la ventana principal y
+     * de la barra de herramientas
+     */
     public void addListeners(){
 
         itemLoad.addActionListener(new ActionListener() {
@@ -136,7 +162,6 @@ public class Window {
             }
         });
 
-
         itemSave.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -155,9 +180,37 @@ public class Window {
             }
         });
 
+        buttonLoad.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.setEnabled(true);
+                textArea.setText("");
+                textArea.setText(fm.openFile());
+                if(!textArea.getText().equalsIgnoreCase("")){
+                    itemSave.setEnabled(true);
+                    itemSaveAs.setEnabled(true);
+                    fileWriteable = true;
+                }
+            }
+        });
 
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(fileWriteable){
+                    fm.saveFile(textArea.getText());
+                }
+            }
+        });
 
-
+        buttonSaveAs.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(fileWriteable){
+                    fm.saveAsFile(textArea.getText());
+                }
+            }
+        });
     }
 
 
